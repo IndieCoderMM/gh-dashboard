23f41107cf23e45cd2882f85ba47d54604658069
hthantoo <hthant00chk@gmail.com>
2023-01-31 16:29:54 +0630

    Add client id

--- src/services/GithubService.js
+++ src/services/GithubService.js
@@ -7,11 +7,25 @@ const http = axios.create({
   },
 });
 
-const getUser = (username) => http.get(`/users/${username}`);
+const GITHUB_CLIENT_ID = process.env.REACT_APP_GITHUB_CLIENT_ID;
+const GITHUB_CLIENT_SECRET = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+
+const getUser = (username) =>
+  http.get(`/users/${username}`, {
+    params: {
+      client_id: GITHUB_CLIENT_ID,
+      client_secret: GITHUB_CLIENT_SECRET,
+    },
+  });
 
 const getRepos = (username) =>
   http.get(`/users/${username}/repos`, {
-    params: { per_page: 100, sort: 'pushed' },
+    params: {
+      per_page: 100,
+      sort: 'pushed',
+      client_id: GITHUB_CLIENT_ID,
+      client_secret: GITHUB_CLIENT_SECRET,
+    },
   });
 
 const GithubService = { getUser, getRepos };
